2d04bf1f75d9eb07094fa804aaeea3c78e4b5098
fedor22i <48512979+fedor22i@users.noreply.github.com>
2021-09-02 13:08:26 +0300

    Start UI

--- Assets/Scripts/MainScreen.cs
+++ Assets/Scripts/MainScreen.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainScreen : MonoBehaviour
 {
     public Canvas OptionScreen;
+    public Canvas mainScreen;
+
+    public void LoadMainScreenFromOptionsScreen()
+    {
+        mainScreen.gameObject.SetActive(true);
+        OptionScreen.gameObject.SetActive(false);
+    }
+
     public void LoadOptionScreen()
     {
-        this.gameObject.SetActive(false);
+        mainScreen.gameObject.SetActive(false);
         OptionScreen.gameObject.SetActive(true);
     }
+
+    public void LoadPlayScreen()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
 }
